1ac1df4ba799cb628d5be82cb3899e69b705df30
ShamayOsmanova <145850579+ShamayOsmanova@users.noreply.github.com>
2023-12-10 17:19:08 +0400

    deyisikler etdim

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return <div className="all">Shop</div>;
 };
 
 export default Shop;
