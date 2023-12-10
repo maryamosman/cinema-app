dfb63145846d2b57f26d236cf0ae0f77d9476f60
ShamayOsmanova <145850579+ShamayOsmanova@users.noreply.github.com>
2023-12-10 18:14:55 +0400

    lorem elave etdim

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        odit officiis accusantium explicabo quae dicta atque aspernatur saepe,
+        delectus repudiandae vel! Voluptatem non pariatur quas similique
+        asperiores dolorum repudiandae fugit.
+      </h1>
+    </div>
+  );
 };
 
 export default About;
